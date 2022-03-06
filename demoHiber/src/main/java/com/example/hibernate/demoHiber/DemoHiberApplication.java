c2362306a5ef291a769f69d2113460a5db42e0fc
Sumithra <blessfulsumi@gmail.com>
2022-03-06 22:51:54 +0530

    first

---
+++ demoHiber/src/main/java/com/example/hibernate/demoHiber/DemoHiberApplication.java
@@ -0,0 +1,13 @@
+package com.example.hibernate.demoHiber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHiberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHiberApplication.class, args);
+	}
+
+}
---
+++ demoHiber/src/test/java/com/example/hibernate/demoHiber/DemoHiberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hibernate.demoHiber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoHiberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
